d7b44b51c175ffcc448604f2daa29cfb4a885b60
Viswa Harini Vidyashankar <viswavidya16@gmail.com>
2024-09-04 23:43:36 -0400

    shows styling

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -30,46 +30,65 @@ let showsList = [
         location: 'San Francisco, CA'
     }
 ]
-let articleEl = document.querySelector('.shows__list');
-console.log(articleEl);
+let showsEl = document.querySelector('.shows');
+// console.log(articleEl);
+
+function labelFunc (label) {
+    // console.log(label);
+    let labelName = document.createElement('p');
+    labelName.innerHTML = label;
+    labelName.classList.add('shows__list-label');
+    return labelName;
+} 
 
 for (let i = 0; i < showsList.length; i++) {
 
-    let show = showsList[i];
+    let articleEl = document.createElement('article');
+    articleEl.classList.add('shows__list');
 
-    let dateLabel = document.createElement('p');
-    dateLabel.innerHTML = 'DATE';
-    dateLabel.classList.add('shows__list-label');
-    articleEl.appendChild(dateLabel);
+
+    let show = showsList[i];   
+   let labelName = labelFunc('DATE');
+    console.log(labelName);
+
+    articleEl.appendChild(labelName);
+
 
     let date = document.createElement('p');
     date.innerHTML = show.date;
-    date.classList.add('shows__list-date');
-    date.classList.add('shows__list-date--highlight');
+    date.classList.add('shows__list-info');
+    date.classList.add('shows__list-info--highlight');
     articleEl.appendChild(date);
 
-    let venueLabel = document.createElement('p');
-    venueLabel.innerHTML = 'VENUE';
-    venueLabel.classList.add('shows__list-label');
-    articleEl.appendChild(venueLabel);
+   let venueName = labelFunc('VENUE');
+
+   articleEl.appendChild(venueName);
+
 
     let venue = document.createElement('p');
     venue.innerHTML = show.venue;
-    venue.classList.add('shows__list-venue');
+    venue.classList.add('shows__list-info');
     articleEl.appendChild(venue);
 
-    let locationLabel = document.createElement('p');
-    locationLabel.innerHTML = 'LOCATION';
-    locationLabel.classList.add('shows__list-label');
-    articleEl.appendChild(locationLabel);
+    let locationName = labelFunc('LOCATION');
+
+    articleEl.appendChild(locationName);
+
 
     let location = document.createElement('p');
     location.innerHTML = show.location;
-    location.classList.add('shows__list-location');
+    location.classList.add('shows__list-info');
     articleEl.appendChild(location);
 
     let buyTickets = document.createElement('button');
     buyTickets.innerHTML = 'BUY TICKETS';
-    buyTickets.classList.add('buyTickets');
+    buyTickets.classList.add('shows__buyTickets');
     articleEl.appendChild(buyTickets);
+
+    showsEl.appendChild(articleEl);
+
+    let divider = document.createElement('hr');
+    divider.classList.add('shows__divider');
+    showsEl.appendChild(divider);
+
 }
